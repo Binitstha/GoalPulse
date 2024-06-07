c7262eeefbcedbb41df9fcfba760bd0256aafdb6
Binitstha <binitcr336@gmail.com>
2024-06-07 14:10:28 +0545

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.map$/,
+      loader: 'ignore-loader',
+    });
+    return config;
+  },
   images: {
     remotePatterns: [{
       protocol: 'https',
--- src/lib/scrapper.ts
+++ src/lib/scrapper.ts
@@ -1,5 +1,6 @@
 import { unstable_noStore } from "next/cache";
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-core";
+import chromium from "chrome-aws-lambda";
 
 interface Articles {
   title: string;
@@ -9,8 +10,19 @@ interface Articles {
 
 export const newsScrapper = async (url: string): Promise<Articles[]> => {
   unstable_noStore();
-  // const browser = await puppeteer.launch();
-  const browser = await puppeteer.launch({ executablePath: "/usr/bin/google-chrome" });
+
+  let browser;
+  if (process.env.NODE_ENV === 'production') {
+    browser = await puppeteer.launch({
+      args: chromium.args,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    });
+  } else {
+    browser = await puppeteer.launch({
+      executablePath: "/usr/bin/google-chrome",
+    });
+  }
 
   const page = await browser.newPage();
   await page.goto(url);
@@ -23,13 +35,14 @@ export const newsScrapper = async (url: string): Promise<Articles[]> => {
       const title = titleElement ? titleElement.innerHTML : "No title";
 
       const imgElement = article.querySelector("img");
-      const img = imgElement ? imgElement?.src : undefined;
+      const img = imgElement ? imgElement.src : undefined;
 
       const timeElement = article.querySelector("time span");
       const time = timeElement ? timeElement.innerHTML : "No time";
       return { title, img, time };
     });
   });
+
   await browser.close();
   return allArticles;
 };
